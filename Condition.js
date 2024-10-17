0319f6576f050969805cce06c80258ee6b5211bb
imamhossain-bd <imamhossain0098@gmail.com>
2024-10-17 12:29:06 +0600

    Js Task Practice

---
+++ Condition.js
@@ -0,0 +1,148 @@
+//........ If Else Condition___________
+
+// const burger = 400;
+// if (burger >= 500) {
+//     console.log("Free Coke")
+// }
+// else{
+//     console.log("Otherwise Buy the coke for 30tk")
+// }
+
+// const weight = 56;
+// const height = 1.5;
+// const bmi = weight / (height * height)
+// console.log(bmi)
+// if (bmi <= 18.5) {
+//     console.log("You Are Underweight")
+// }
+// else if(bmi >= 18.5 && bmi <= 24.9){
+//     console.log("You are Normal")
+// }
+// else if(bmi >= 25 && bmi <= 29.9){
+//     console.log("You are Overweight")
+// }
+// else{
+//     console.log("Otherwise, You are obese")
+// }
+
+
+// const food = 800;
+// const age = 61;
+
+// if(age <= 10){
+//     console.log("Children food free")
+// }
+// else if(age >= 60){
+//     const discount = food * 50 /100
+//     const foodPrice = food - discount
+//     console.log("Food Price: " , foodPrice)
+// }
+// else if(age >= 40 && age <= 50){
+//     const discount = food * 20 /100
+//     const foodPrice = food - discount
+//     console.log("Food Price:", foodPrice)
+// }
+// else{
+//     console.log("Regular Food Price: ", food)
+// }
+
+//........ Looping Task ______________
+
+// let msg = 0;
+// while (msg <= 30) {
+//     console.log(msg, "Shek Hasina Palay na")
+//     msg++
+// }
+
+
+// let odd = 0;
+// while(odd <= 40){
+//     if(odd % 2 == 1){
+//         console.log("Odd Number", odd)
+//     }
+//     odd++
+// }
+
+
+// let even = 0
+// while(even <= 50){
+//     if(even % 2 == 0){
+//         console.log("Even Number ", even)
+//     }
+//     even++
+// }
+
+
+// let odd = 81;
+// let sum = 0
+// while(odd <= 131){
+//     if(odd % 2 == 1){
+//         console.log("Odd Number", odd)
+//         sum = sum + odd
+//         console.log("Total sum:",sum)
+//     }
+//     odd++
+// }
+
+// let even = 206;
+// let sum1 = 0;
+
+// while(even <= 311){
+//     if(even % 2 == 0){
+//         console.log("Even Number", even)
+//         sum1 = sum1 + even;
+//         console.log("Sum Number",sum1)
+//     }
+//     even++
+// }
+
+// let sum = 0
+// for(let even = 206; even <= 311; even++){
+//     if(even % 2 == 0){
+//         console.log("This even Number", even)
+//     }
+//     sum = sum + even; 
+// }
+// console.log("Even number total", sum)
+
+
+// let multiply = 1;
+// let sum = 45;
+// while(multiply <= 10){
+//     let multi = sum + "X" + multiply + "=" + (multiply * sum);
+//     console.log(multi)
+//     multiply++
+// }
+
+// let sum3 = 36;
+// for(let multiply = 1; multiply <= 10; multiply++){
+//     let sum = sum3 + "X" + multiply + "=" + (multiply * sum3)
+//     console.log(sum)
+// }
+
+
+// let countDown = 21;
+// while(countDown >= 10){
+//     console.log("Baby Come down :", countDown)
+//     countDown--
+// }
+
+// for (let countDown = 21; countDown >= 10 ; countDown--) {
+//     console.log("Count Down", countDown)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
